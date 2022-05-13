1ead739cb732fe889f762177707a22459e2f03bc
paulahemsi <paulahemsi@gmail.com>
2022-05-12 21:11:46 -0300

    chore(mariadb): wordpress db configuration -mariadb script- ok

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,16 +4,15 @@ DATABASE_PATH=/var/lib/mysql/$MYSQL_DATABASE
 
 if [ ! -d "$DATABASE_PATH" ]
 then
-	mkdir -p /var/run/mysqld;
-	chown -R mysql:mysql /var/run/mysqld;
-	chmod 777 /var/run/mysqld;
-
+	echo "creating database";
 	service mysql start;
 	mysql -u root --execute="CREATE DATABASE $MYSQL_DATABASE; \
 				 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; \
 				 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
-				 GRANT ALL PRIVILAGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';";	
+				 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';";
+	echo "database created";
 	service mysql stop;
+	echo "mysql stoped";
 fi
 
 exec "$@"

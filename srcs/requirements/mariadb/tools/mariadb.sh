84765df1e2f809fa12006727c98fb3cbf8c3eabf
paulahemsi <paulahemsi@gmail.com>
2022-05-14 12:50:01 -0300

    de 403 para 404

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -15,7 +15,4 @@ then
 	echo "mysql stoped";
 fi
 
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
 exec "$@"

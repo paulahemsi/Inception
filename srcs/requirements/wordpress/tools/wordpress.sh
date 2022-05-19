19a7096a40bfb088097ba566b074deadea3db239
paulahemsi <paulahemsi@gmail.com>
2022-05-18 21:25:53 -0300

    Error establishing a database connection or: small victories

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -4,16 +4,11 @@ WP_CONFIG=/var/www/phemsi-a/wordpress/wp-config.php
 
 if [ ! -f "$WP_CONFIG" ]
 then
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm latest.tar.gz
-
-	cd /var/www/phemsi-a/wordpress
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	mv wp-config-sample.php $WP_CONFIG
+	sed -i "s/username_here/$MYSQL_USER/g" $WP_CONFIG 
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" $WP_CONFIG 
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" $WP_CONFIG 
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" $WP_CONFIG 
 fi
 
 exec "$@"

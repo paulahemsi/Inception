81d976a1303daaae108537389c3d46a938b005e4
paulahemsi <paulahemsi@gmail.com>
2022-05-21 14:06:45 -0300

    replace local host to % mariadb script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,15 +4,13 @@ DATABASE_PATH=/var/lib/mysql/$MYSQL_DATABASE
 
 if [ ! -d "$DATABASE_PATH" ]
 then
-	echo "creating database";
 	service mysql start;
 	mysql -u root --execute="CREATE DATABASE $MYSQL_DATABASE; \
-				 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+				 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
 				 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
-				 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';";
-	echo "database created";
+				 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';";	
 	service mysql stop;
-	echo "mysql stoped";
+	
 fi
 
 exec "$@"

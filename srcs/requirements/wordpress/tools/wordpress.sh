39f2e4f2eb0c32c27719cfb8a5204acc6bbb1c36
paulahemsi <paulahemsi@gmail.com>
2022-05-21 14:04:44 -0300

    wp script minor changes

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-WP_CONFIG=/var/www/phemsi-a/wordpress/wp-config.php
+WP_CONFIG=wp-config.php
 
 if [ ! -f "$WP_CONFIG" ]
 then
 	mv wp-config-sample.php $WP_CONFIG
 	sed -i "s/username_here/$MYSQL_USER/g" $WP_CONFIG 
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" $WP_CONFIG 
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" $WP_CONFIG 
+	sed -i "s/localhost/$MYSQL_HOST/g" $WP_CONFIG 
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" $WP_CONFIG 
 fi
 
